4f692e974c389d140f6dc8e16af4b9c3e44bf1b1
cshionis@gmail.com <cshionis@gmail.com>
2023-08-30 13:09:56 +0000

    updating stg and int transaction models - removing unnecessary fields

--- models/intermediate/int_transactions_pivoted_to_customers.sql
+++ models/intermediate/int_transactions_pivoted_to_customers.sql
@@ -9,7 +9,6 @@ from {{ ref('stg_transactions') }}
     t.transaction_id
   , t.customer_id
   , t.transaction_date
-  , t.transaction_month
   , t.amount
   , date_diff(t.transaction_date, lag(t.transaction_date) over (partition by t.customer_id order by t.transaction_date), day) transaction_interval_in_days
   from transactions t 
@@ -21,9 +20,8 @@ from {{ ref('stg_transactions') }}
   , count(*) as transactions_to_date
   , round(avg(transaction_interval_in_days),2) as avg_transaction_interval_in_days
   , min(transaction_date) as first_transaction_date
-  , min(transaction_month) as first_transaction_month
-  , max(transaction_month) as last_transaction_month
-  , date_diff(max(transaction_month),min(transaction_month),month)+1 number_of_months_active
+  , max(transaction_date) as most_recent_transaction_date
+  , date_diff(max(transaction_date),min(transaction_date),month)+1 number_of_months_active
   , sum(amount) as total_amount
   from transactions_prep
   group by 1
@@ -32,6 +30,7 @@ from {{ ref('stg_transactions') }}
   select
     customer_id
   , first_transaction_date
+  , most_recent_transaction_date
   , transactions_to_date
   , round(total_amount / number_of_months_active,2) as avg_monthly_spending
   , avg_transaction_interval_in_days
--- models/staging/stg_transactions.sql
+++ models/staging/stg_transactions.sql
@@ -9,7 +9,6 @@ select
 , customer_id
 -- dates
 , cast(REGEXP_REPLACE(transaction_date, r'^[^-]+', '2023') as date) as transaction_date
-, date_trunc(cast(REGEXP_REPLACE(transaction_date, r'^[^-]+', '2023') as date),month) as transaction_month
 --numeric
 , amount
 from source
